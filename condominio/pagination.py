cf769190227d7e1e78f2ce87e9c7e86f27a1336f
Severitech <padilladouglas6@gmail.com>
2025-11-27 00:04:44 -0400

    produccion

--- condominio/pagination.py
+++ condominio/pagination.py
@@ -2,9 +2,9 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class ReservaPagination(PageNumberPagination):
-    page_size = 100  # o 10
+    page_size = 20  # o 10
     page_size_query_param = 'page_size'
-    max_page_size = 1000
+    max_page_size = 100
 
     def get_paginated_response(self, data):
         # Solo devuelve la lista de datos, sin metadatos
